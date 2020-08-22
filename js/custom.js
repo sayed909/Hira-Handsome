75e835ffb36d2b6724a8bbb514e6e69ecbe43b51
sayed909 <69435597+sayed909@users.noreply.github.com>
2020-08-22 14:17:24 +0300

    hirahansome

---
+++ js/custom.js
@@ -0,0 +1,14 @@
+jQuery(document).ready(function () {
+    jQuery(".owl-carousel").owlCarousel({
+        'items': 5,
+        'loop': true,
+        'center': true,
+        'autoplay': true,
+        'autoplayTimeout': 3000,
+        'smartSpeed': 800,
+        'autoplaySpeed': 2000,
+        'navSpeed': 800,
+        'animateOut': true,
+        'animateIn': true
+    });
+});
